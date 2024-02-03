6507484cd496aa457bf0408287520f9bc6d6e752
Tiago <tiago.guerreiro76@gmail.com>
2024-02-03 16:56:10 -0300

    chore: removed unnecessary installation steps, added custom agw_install_ubuntu script

--- install_agw.sh
+++
@@ -1,4 +0,0 @@
-echo -e "${GREEN}---------------------Starting AGW installation..."
-
-wget https://raw.githubusercontent.com/magma/magma/master/lte/gateway/deploy/agw_install_ubuntu.sh
-bash agw_install_ubuntu.sh
--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 bash magma_dependencies_v2.sh
-bash network_config.sh
-bash install_agw.sh
+bash resources/agw_install_ubuntu.sh
