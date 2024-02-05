3bf68ef16263889f92185829b7e01c752c88809b
tiagoguerreirodev <tiago.guerreiro76@gmail.com>
2024-02-05 15:47:34 -0300

    ref: log a message in the terminal, informing the user that the computer will reboot

--- resources/agw_install_ubuntu.sh
+++ resources/agw_install_ubuntu.sh
@@ -209,6 +209,9 @@ if [ "$MAGMA_INSTALLED" != "$SUCCESS_MESSAGE" ]; then
   echo "AGW installation is done, Run agw_post_install_ubuntu.sh install script after reboot to finish installation"
   wget https://raw.githubusercontent.com/magma/magma/"$MAGMA_VERSION"/lte/gateway/deploy/agw_post_install_ubuntu.sh -P /root/
 
+  echo "Rebooting in 10s..."
+  sleep 10
+
   reboot
 else
   echo "Magma already installed, skipping.."
